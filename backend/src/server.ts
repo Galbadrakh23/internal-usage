335299f2820190fc87da43010d71cc9e21fb353f
galbadrakh <galbadrak223@gmail.com>
2025-02-18 11:54:58 +0800

    bck deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,10 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
+app.get("/", (_, res) => {
+  res.send("Welcome Internal Usage API");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port : ${PORT}`);
 });
